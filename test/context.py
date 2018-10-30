b4b01c8d400cdb632f91f0b704f63c02723f514f
CareF <CareF.Lm@gmail.com>
2018-10-30 15:17:07 -0400

    update test context

--- test/context.py
+++ test/context.py
@@ -5,6 +5,8 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-#  print(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+if __name__ == "__main__":
+    print(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # vim: ts=4 sw=4 sts=4 expandtab
