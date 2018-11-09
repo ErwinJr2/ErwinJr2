ebea1d16a271857a7829f5a5f7a220acee22e88d
CareF <CareF.Lm@gmail.com>
2018-11-09 12:00:23 -0500

    init unit test

---
+++ test/SimpleSchrodinger.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from context import *
+from OneDQuantum import *
+import numpy as np
+import unittest
+from scipy.constants import hbar, e, m_e, pi
+ANG=1E-10
+
+#test precision
+wf_dec = 3
+E_dec  = 6
+class TestSimpleSchrodinger(unittest.TestCase):
+    def test_inf_sq_well(self):
+        xmax = 200
+        x = np.linspace(0, xmax, 1000)
+        mass = 0.067
+        step = x[1] - x[0]
+        Es = np.linspace(0, 0.4, 100)
+        V = np.zeros(x.shape)
+        EigenEs = cSimpleSolve1D(step, Es, V, mass)
+        psis = cSimpleFillPsi(step, EigenEs, V, mass)
+
+        # Theoretical result
+        an = np.arange(1,5)
+        EigenEs_th = hbar**2/(2*m_e*mass)*an**2*pi**2/(xmax*ANG)**2/e
+        psis_th = np.sqrt(2/xmax) * np.sin(np.outer(an, x) * pi / xmax)
+
+        np.testing.assert_array_almost_equal(EigenEs[:len(an)], EigenEs_th, 
+                                             decimal=E_dec, verbose=True)
+        np.testing.assert_array_almost_equal(psis[:len(an),:], psis_th,
+                                             decimal=wf_dec, verbose=True)
+
+    def test_trangle_well(self):
+        F = 2e-4
+        xmax = 1E3
+        mass = 0.067  # Typical parameters for GaAs
+        x = np.linspace(0, xmax, 5000)
+        step = x[1] - x[0]
+        V = F * (xmax - x)
+        Es = np.linspace(0, 0.15, 100)
+        EigenEs = cSimpleSolve1D(step, Es, V, mass)
+        psis = cSimpleFillPsi(step, EigenEs, V, mass)
+
+        # Theoretical result
+        nmax = 8
+        from scipy.special import airy, ai_zeros
+        an = ai_zeros(nmax)[0]
+        EigenEs_th = -(hbar**2*F**2/(2*m_e*mass*e*ANG**2))**(1/3)*an
+        psis_th = np.array([airy((2*mass*m_e*e*ANG**2*F/hbar**2)**(1/3) 
+                                 * (x-E/F))[0] for E in EigenEs_th])
+        psis_th /= (np.linalg.norm(psis_th, axis=1) * np.sqrt(step))[:, None]
+
+        np.testing.assert_array_almost_equal(EigenEs[:nmax], EigenEs_th, 
+                                             decimal=E_dec, verbose=True)
+        np.testing.assert_array_almost_equal(psis[:nmax,::-1], psis_th,
+                                             decimal=wf_dec, verbose=True)
+
+if __name__ == "__main__":
+    unittest.main()
+# vim: ts=4 sw=4 sts=4 expandtab
