try populate more, calculation might be wrong

QCLayers.py
@@ -81,8 +81,15 @@ class QCLayers(object):
                                      self.layerDopings[n]
             self.xARs[Indices] = self.xARs[Indices] * self.layerARs[n]
             self.xLayerNums[Indices] = self.xLayerNums[Indices] * n
-            
-            
+
+        self.xVc = np.zeros_like(self.xPoints, dtype=float)
+        self.xEg = np.zeros_like(self.xPoints, dtype=float)
+
+        # change indx to the indices with the same type of meterials
+        for indx in range(Max_grid_ind+1):
+            self.xVc[indx] = (self.layerMaterials[self.xLayerNums[indx]].parm['EcG']
+                              - self.xPoints[indx] * self.EField )
+            self.xEg[indx] = (self.layerMaterials[self.xLayerNums[indx]].parm['EgLH'])
 
 
 # vim: ts=4 sw=4 sts=4 expandtab