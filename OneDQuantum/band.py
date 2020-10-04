4ab5420aa3de8f51dc0997e184459bca6736513e
CareF <CareF.Lm@gmail.com>
2020-10-03 23:18:43 -0400

    band type hint

--- OneDQuantum/band.py
+++ OneDQuantum/band.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 import numpy as np
-from ctypes import c_void_p, c_int, c_double, POINTER, Structure
+from ctypes import c_void_p, c_int, c_double, POINTER, Structure, CDLL
+from typing import Callable, Union
 __all__ = ['Band', 'cUpdateBand']
 from .typeDefs import doubleArray
 
@@ -12,36 +13,13 @@ class cBand(Structure):
                 ("Eg", POINTER(c_double))]
 
 
-def init(clib):
-    global cUpdateBand, cZBband_new, cZBband_free
-    clib.UpdateBand.argtypes = [POINTER(cBand), c_double,
-                                doubleArray, doubleArray]
-    clib.UpdateBand.restype = c_int
-
-    def cUpdateBand(band: Band, E: float, V: np.ndarray, m: np.ndarray) -> int:
-        return clib.UpdateBand(band.c, c_double(E), V, m)
-
-    clib.ZBband_new.argtypes = [c_int, doubleArray, doubleArray,
-                                doubleArray, doubleArray]
-    clib.ZBband_new.restype = POINTER(cBand)
-
-    def cZBband_new(xEg: np.ndarray, xF: np.ndarray, xEp: np.ndarray,
-                    xESO: np.ndarray) -> POINTER(cBand):
-        return clib.ZBband_new(xEg.size, xEg, xF, xEp, xESO)
-
-    clib.ZBband_free.argtypes = [POINTER(cBand)]
-    clib.ZBband_free.restype = None
-
-    def cZBband_free(zbband: POINTER(cBand)) -> None:
-        return clib.ZBband_free(zbband)
-
-    return cUpdateBand, cZBband_new, cZBband_free
+CBAND_P = POINTER(cBand)
 
 
 class Band(object):
     """Python interface for a Band"""
     bandtype: str
-    c: POINTER(cBand)
+    c: CBAND_P
 
     def __init__(self, bandtype: str, *args, **kwargs):
         super(Band, self).__init__()
@@ -68,11 +46,40 @@ def cUpdateBand(band: Band, E: float, V: np.ndarray, m: np.ndarray) -> int:
 
 
 def cZBband_new(xEg: np.ndarray, xF: np.ndarray, xEp: np.ndarray,
-                xESO: np.ndarray) -> POINTER(cBand):
+                xESO: np.ndarray) -> CBAND_P:
     raise NotImplementedError("Not linked to a proper clib.")
 
 
-def cZBband_free(zbband: POINTER(cBand)) -> None:
+def cZBband_free(zbband: CBAND_P) -> None:
     raise NotImplementedError("Not linked to a proper clib.")
+
+
+def init(clib: CDLL) -> Union[
+        Callable[[Band, float, np.ndarray, np.ndarray], int],
+        Callable[[np.ndarray, np.ndarray, np.ndarray, np.ndarray], CBAND_P],
+        Callable[[CBAND_P], None]]:
+    global cUpdateBand, cZBband_new, cZBband_free
+    clib.UpdateBand.argtypes = [POINTER(cBand), c_double,
+                                doubleArray, doubleArray]
+    clib.UpdateBand.restype = c_int
+
+    def cUpdateBand(band: Band, E: float, V: np.ndarray, m: np.ndarray) -> int:
+        return clib.UpdateBand(band.c, c_double(E), V, m)
+
+    clib.ZBband_new.argtypes = [c_int, doubleArray, doubleArray,
+                                doubleArray, doubleArray]
+    clib.ZBband_new.restype = POINTER(cBand)
+
+    def cZBband_new(xEg: np.ndarray, xF: np.ndarray, xEp: np.ndarray,
+                    xESO: np.ndarray) -> POINTER(cBand):
+        return clib.ZBband_new(xEg.size, xEg, xF, xEp, xESO)
+
+    clib.ZBband_free.argtypes = [POINTER(cBand)]
+    clib.ZBband_free.restype = None
+
+    def cZBband_free(zbband: POINTER(cBand)) -> None:
+        return clib.ZBband_free(zbband)
+
+    return cUpdateBand, cZBband_new, cZBband_free
 
 # vim: ts=4 sw=4 sts=4 expandtab
