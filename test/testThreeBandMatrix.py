3396d614ff3d275c4963940a3f611f7408b1a26e
CareF <CareF.Lm@gmail.com>
2021-04-06 21:11:03 -0400

    update matrix tests

--- test/testThreeBandMatrix.py
+++ test/testThreeBandMatrix.py
@@ -14,6 +14,7 @@ class TestThreeBandMatrix(unittest.TestCase):
         mtrls = [1, 0, 1, 0, 1, 0, 1]
         qcLayers = QCLayers(xres=0.01, layerWidths=layers,
                             layerMtrls=mtrls, repeats=1, T=10.0)
+        qcLayers.solver = 'ODE'
         qcLayers.populate_x()
         qcLayers.solve_whole()
         psis_ode, eigenEs_ode = qcLayers.psis, qcLayers.eigenEs
@@ -31,49 +32,25 @@ class TestThreeBandMatrix(unittest.TestCase):
         np.testing.assert_almost_equal(phiso_ode, phiso_mtx, decimal=3)
         self.assertAlmostEqual(dipole_ode, dipole_mtx, places=3)
 
-
-def plot_debugger():
-    import matplotlib.pyplot as plt
-    layers = [300, 46, 10, 20, 10, 19, 300]
-    mtrls = [1, 0, 1, 0, 1, 0, 1]
-    qcLayers = QCLayers(xres=0.01, layerWidths=layers,
-                        layerMtrls=mtrls, repeats=1, T=10.0)
-    qcLayers.populate_x()
-    qcLayers.solve_whole()
-    psis_ode, eigenEs_ode = qcLayers.psis, qcLayers.eigenEs
-    philh_ode, _ = qcLayers.philh, qcLayers.phiso
-    print(psis_ode, eigenEs_ode)
-    qcLayers.solver = 'matrix'
-    qcLayers.matrixEigenCount = 4
-    qcLayers.solve_whole()
-    psis_mtx, eigenEs_mtx = qcLayers.psis, qcLayers.eigenEs
-    print(psis_mtx, eigenEs_mtx)
-    print(qcLayers.eigenEs)
-    print(np.diff(qcLayers.eigenEs))
-    print(qcLayers.dipole(0, 1))
-    # [0.12559815 0.24226628 0.38040656 0.48960184]
-    # [0.11666813 0.13814028 0.10919528]
-    # 17.735693339174034
-
-    # qcLayers = singleWell
-    plt.xlabel('Position (Å)')
-    plt.ylabel('Energy (eV)')
-    xEg, xF, xEp, xESO = qcLayers.bandParams
-    plt.plot(qcLayers.xPoints, qcLayers.xVc, 'k', linewidth=1)
-    # plt.plot(qcLayers.xPoints, qcLayers.xVc-xEg, 'k', linewidth=1)
-    # plt.plot(qcLayers.xPoints, qcLayers.xVc-xEg-xESO, 'k', linewidth=1)
-    for n in range(qcLayers.eigenEs.size):
-        plt.plot(qcLayers.xPoints,
-                 10*qcLayers.psis[n, :] + qcLayers.eigenEs[n])
-        plt.plot(qcLayers.xPoints,
-                 10*psis_ode[n, :] + qcLayers.eigenEs[n], '--')
-        plt.plot(qcLayers.xPoints,
-                 10*qcLayers.philh[n, :] + qcLayers.eigenEs[n])
-        plt.plot(qcLayers.xPoints,
-                 10*philh_ode[n, :] + qcLayers.eigenEs[n], '--')
-    plt.show()
+    def test_three_well_biased(self):
+        layers = [300, 46, 10, 20, 10, 19, 300]
+        mtrls = [1, 0, 1, 0, 1, 0, 1]
+        qcLayers = QCLayers(xres=0.01, layerWidths=layers, EField=30,
+                            layerMtrls=mtrls, repeats=1, T=10.0)
+        qcLayers.solver = 'ODE'
+        qcLayers.populate_x()
+        qcLayers.solve_whole()
+        psis_ode, eigenEs_ode = qcLayers.psis[:4], qcLayers.eigenEs[:4]
+        dipole_ode = qcLayers.dipole(0, 1)
+        qcLayers.solver = 'matrix'
+        qcLayers.matrixEigenCount = 4
+        qcLayers.solve_whole()
+        psis_mtx, eigenEs_mtx = qcLayers.psis, qcLayers.eigenEs
+        dipole_mtx = qcLayers.dipole(0, 1)
+        np.testing.assert_almost_equal(eigenEs_ode, eigenEs_mtx, decimal=5)
+        np.testing.assert_almost_equal(psis_ode, psis_mtx, decimal=5)
+        self.assertAlmostEqual(dipole_ode, dipole_mtx, places=3)
 
 
 if __name__ == "__main__":
     unittest.main()
-    # plot_debugger()
