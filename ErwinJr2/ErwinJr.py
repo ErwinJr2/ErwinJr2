2b53238762b4c6320f13480bdcba95aebdc0bfbe
CareF <CareF.Lm@gmail.com>
2021-04-14 21:28:33 -0400

    default tutorial to stable

--- ErwinJr2/ErwinJr.py
+++ ErwinJr2/ErwinJr.py
@@ -557,7 +557,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
         if os.path.exists(path):
             QDesktopServices.openUrl(QUrl("file://" + os.path.abspath(path)))
         else:
-            QDesktopServices.openUrl(QUrl("https://erwinjr2.readthedocs.io/"))
+            QDesktopServices.openUrl(
+                QUrl("https://erwinjr2.readthedocs.io/en/stable/"))
 
 
 def main(fileName=None):
--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ See https://erwinjr2.readthedocs.io/ for details.
 
 setup(
     name='ErwinJr2',
-    version='2.2.0',
+    version='2.2.1',
     author='Ming Lyu',
     author_email='minglyu@princeton.edu',
     license="GPL-3.0",
