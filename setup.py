ece06d4ad1c9df423497c4a8135c4b5cf84aaf39
CareF <CareF.Lm@gmail.com>
2024-09-09 15:44:14 +0800

    update packaging setting

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ import subprocess
 import warnings
 
 from setuptools import setup
+from setuptools.command.build_py import build_py
 from setuptools.command.develop import develop
+from setuptools.dist import Distribution
 
 
 def build_clib():
@@ -48,8 +50,22 @@ class EJDevelopCMD(develop):
         super().run()
 
 
+class EJBuildCMD(build_py):
+    def run(self):
+        build_clib()
+        super().run()
+
+
+class EJDistribution(Distribution):
+    def has_ext_modules(self):
+        super().has_ext_modules()
+        return True
+
+
 setup(
     cmdclass={
-        'develop': EJDevelopCMD
+        'develop': EJDevelopCMD,
+        'build_py': EJBuildCMD,
     },
+    distclass=EJDistribution,
 )
